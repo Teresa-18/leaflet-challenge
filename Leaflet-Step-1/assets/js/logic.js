afe8608f275849c05b647765430d4e57523cdc1e
teresa luna <tmluna_18@outlook.com>
2021-02-14 16:14:28 -0600

    adding code to try to change markers depending on mag size, NOT WORKING YET

--- Leaflet-Step-1/assets/js/logic.js
+++ Leaflet-Step-1/assets/js/logic.js
@@ -3,31 +3,30 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+
   console.log(data.features); 
 });
 
-function createFeatures(earthquakeData) { // *** earthquakeData is the DATA coming from up above's query ***
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeaturePrep(feature, layer) { // **** GRAB only what is needed from the DATA *****
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+var magMarkers = [];
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, { // *** CALL the geoJSON function on the DATA
-    onEachFeature: onEachFeaturePrep
-  });
+// Loop through locations and create city and state markers
+for (var i = 0; i < data.length; i++) {
+  magMarkers.push(
+    L.circle(feature.geometry.coordinates, {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "white",
+      radius: markerSize((feature.properties.mag)*10000)
+    }).bindPopup("<h3> Location: " + feature.properties.place +
+    "</h3><hr><p> Time: " + new Date(feature.properties.time) + "</p>"+
+    "</hr><hr><p> Magnitude: " + feature.properties.mag + "</p>")
+  );
+};
+var earthquakes = L.layerGroup(magMarkers);
 
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
+function createMap() {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
